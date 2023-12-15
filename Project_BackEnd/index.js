915f8749dd37fb22327cdc9c5e47c564f9af0b9e
Diego Goncalves <diego.goncalves1106@hotmail.com>
2023-12-15 07:53:34 -0300

    Importando sequelize e association no index

--- Project_BackEnd/index.js
+++ Project_BackEnd/index.js
@@ -2,14 +2,23 @@ import { express } from "express";
 import { pkg } from "body-parser";
 import { router } from "./routes/router.js";
 
+import sequelize from "./utils/database.js";
+import association from "./models/Association.js";
 const app = express();
-const { json, urlencoded}= pkg
+const { json, urlencoded } = pkg;
 
 app.use(json());
-app.use( urlencoded({extends:true}));
+app.use(urlencoded({ extends: true }));
 
-app.listen(3000, ()=>{
-    console.console.log("Escutando porta 3000");
-})
-
-app.use("/", router);
+(async () => {
+  try {
+    association.associations(); 
+    await sequelize.sync();
+    app.use("/", router);
+    app.listen(3000, () => {
+      console.console.log("Escutando porta 3000");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+})();
